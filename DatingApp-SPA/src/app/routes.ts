65cf01999f641fb664e8042ee232e22b1d1a25bc
namnguyen191 <38139823+namnguyen191@users.noreply.github.com>
2020-03-25 21:40:52 -0400

    End of Section 7

---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+// Ordering is important
+export const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    // Dummy route for safe guard a punch of group together
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent },
+            { path: 'messages', component: MessagesComponent },
+            { path: 'lists', component: ListsComponent }
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+];
