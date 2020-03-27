879e6017cb6bf185680384521c8c1e47443f58d5
namnguyen191 <38139823+namnguyen191@users.noreply.github.com>
2020-03-26 21:59:20 -0400

    End of Section 8

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -15,7 +15,7 @@ namespace DatingApp.API.Data
         }
         public async Task<User> Login(string username, string password)
         {
-            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username.ToLower() == username.ToLower());
 
             if(user == null){
                 return null;
